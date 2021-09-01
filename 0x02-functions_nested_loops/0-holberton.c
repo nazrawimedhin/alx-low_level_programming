8c1f73443e0c8e124c7a147c7db6cd01bac136ce
nazrawi <nazrawimedhin1@gmail.com>
2021-09-01 21:25:44 +0300

    all

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -13,15 +13,13 @@
  */
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
 	_putchar('t');
-	_putchar('o');
-	_putchar('n');
-
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	return (0);
 }
