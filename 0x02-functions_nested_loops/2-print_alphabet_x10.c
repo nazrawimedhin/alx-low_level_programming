e49c4166f66def2d8c79902887331a41b79f74ab
nazrawi <nazrawimedhin1@gmail.com>
2021-09-01 21:42:58 +0300

    all

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,22 +1,20 @@
-/*
- * File: 2-print_alphabet_x10.c
- * Auth: Nazrawi Medhin
- */
-
 #include "holberton.h"
 
 /**
- * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
+ * print_alphabet_x10 - Prints the alphabet 10 times.
+ *
+ * Return: Always 0 (Success)
  */
 void print_alphabet_x10(void)
 {
-	int count = 0;
-	char letter;
+	int a, b;
 
-	while (count++ <= 9)
+	for (a = 0; a <= 9; a++)
 	{
-		for (letter = 'a'; letter <= 'z'; letter++)
-			_putchar(letter);
-		_putchar('\n');
+		for (b = 97; b <= 122; b++)
+		{
+			_putchar(b);
+		}
+	_putchar('\n');
 	}
 }
